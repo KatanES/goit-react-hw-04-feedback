f2ec1430138ace997e7254e735918ae02a89c0a8
KatanES <businesskatan.es@gmail.com>
2023-08-25 22:43:37 +0200

    refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,58 @@
-import { Component } from 'react';
 import { Section } from './Section';
 import { FeedbackOptions } from './Feedback/Feedback';
 import { Statistics } from './StatisticsList';
 import { Notification } from './Feedback/Notification';
 import { FeedbackSection } from './Feedback/Feedback.styled';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleFeedback = type => {
+    switch (type) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
-  handleFeedback = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-    }));
-  };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = good + neutral + bad;
-    const positiveFeedbackPercentage = totalFeedback
-      ? Math.round((good / totalFeedback) * 100)
-      : 0;
+  const totalFeedback = good + neutral + bad;
+  const positiveFeedbackPercentage = totalFeedback
+    ? Math.round((good / totalFeedback) * 100)
+    : 0;
 
-    return (
-      <FeedbackSection>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleFeedback}
+  return (
+    <FeedbackSection>
+      <Section title="Будь ласка, залиште відгук">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Статистика">
+        {totalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbackPercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {totalFeedback > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </FeedbackSection>
-    );
-  }
-}
+        ) : (
+          <Notification message="Немає відгуків" />
+        )}
+      </Section>
+    </FeedbackSection>
+  );
+};
